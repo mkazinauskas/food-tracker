092505f9a58280469af02f1324bfa6b5fe4a83f2
Modestas <m.kazinauskas@gmail.com>
2020-11-16 22:00:40 +0200

    Nice picture preview

--- lib/ui/screens/addnew/add_new_screen.dart
+++ lib/ui/screens/addnew/add_new_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:foodtracker/main.dart';
 import 'package:foodtracker/ui/navigation_bar.dart';
 import 'package:foodtracker/ui/screens/addnew/take_picture_screen.dart';
@@ -19,7 +20,7 @@ class _AddNewScreenState extends State<AddNewScreen> {
   String _imagePath = '';
   bool _descriptionAdded = false;
 
-  void _onTakePhoto (@required imagePath) {
+  void _onTakePhoto(@required imagePath) {
     setState(() {
       _photoTaken = true;
       _imagePath = imagePath;
@@ -28,29 +29,46 @@ class _AddNewScreenState extends State<AddNewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    if(!_photoTaken){
-      return TakePictureScreen(camera: widget.firstCamera, callback: _onTakePhoto);
+    if (!_photoTaken) {
+      return TakePictureScreen(
+          camera: widget.firstCamera, callback: _onTakePhoto);
     }
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Add New'),
-        ),
-        body:  Center(
-            child: Column(
+      appBar: AppBar(
+        title: const Text('Add New'),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
               children: [
-                Text('Add New screen'),
-                Image.file(File(_imagePath))
+                buildFoodPhoto(constraints),
               ],
-            )),
-        bottomNavigationBar: NavigationBar(currentScreen: Routes.ADD_NEW),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(currentScreen: Routes.ADD_NEW),
     );
+  }
 
-
-
-
-
-
-
-
+  Widget buildFoodPhoto(BoxConstraints constraints) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _photoTaken = false;
+            });
+          },
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.file(
+              File(_imagePath),
+              height: constraints.maxHeight / 2,
+            ),
+          )),
+    );
   }
 }
